409cb0cb01e13f505591958bb880b4f004f4127b
Gabriel Mouallem <gabriel.unifei2017@gmail.com>
2025-09-18 21:44:54 -0300

    refactor: update authentication flow and structures removing supabase based auth in favor of better-auth

--- lib/auth/server.ts
+++ lib/auth/server.ts
@@ -2,6 +2,7 @@ import { betterAuth } from "better-auth";
 import { Pool } from "pg";
 
 export const auth = betterAuth({
+  baseURL: `${process.env.NEXT_PUBLIC_BETTER_AUTH_URL}/api/github/callback`,
   database: new Pool({
     connectionString: process.env.SUPABASE_DB_CONN_URI!,
   }),
