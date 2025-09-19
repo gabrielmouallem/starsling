e0a9a8f1dd54e662bf0bcecf24497c6c0c0e1e35
Gabriel Mouallem <gabriel.unifei2017@gmail.com>
2025-09-19 14:51:19 -0300

    refactor: update CreateOrganizationDialog to use React.Node for trigger and simplify click handling

--- components/create-organization-dialog.tsx
+++ components/create-organization-dialog.tsx
@@ -13,21 +13,20 @@ export function CreateOrganizationDialog({
   trigger,
   children,
 }: {
-  trigger: React.ReactElement;
+  trigger: React.ReactNode;
   children: React.ReactNode;
 }) {
   const [open, setOpen] = React.useState(false);
 
-  const enhancedTrigger = React.cloneElement(trigger, {
-    onClick: (e: any) => {
-      trigger.props?.onClick?.(e);
-      setOpen(true);
-    },
-  });
-
   return (
     <>
-      {enhancedTrigger}
+      <span
+        onClick={() => setOpen(true)}
+        aria-haspopup="dialog"
+        className="inline-block"
+      >
+        {trigger}
+      </span>
       <Dialog open={open} onOpenChangeAction={setOpen}>
         <DialogContent className="p-0 overflow-hidden">
           <div className="p-4 border-b">
